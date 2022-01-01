95c9719ed2a44b3e0a3d58b2836be1c3961b6357
moutaz <moutazmuhammad1997@gmail.com>
2022-01-01 04:30:44 +0200

    Add create_table.sh

---
+++ create_table.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+shopt -s extglob
+
+path=~/my_dbms/$select_name
+
+function createTable
+{
+echo -e "\nEnter Table name: "
+read table_name
+
+case $table_name in
+	+([a-zA-Z])) # work by using [shopt -s extglob]
+		if [ ! -f $path/$table_name ]
+		then
+			touch ~/my_dbms/$select_name/$table_name;
+			
+			echo -e 'Table Created Successfully.\n'
+			sleep 1
+			. ./use_db.sh
+		else 
+			echo "Table ($table_name) is Exist."
+			sleep 1
+			createTable
+		fi 
+			;;
+	*) echo -e "\nTable name can not contain number or spesial characters.\nplease, Try again..."	  
+	   createTable ;;
+esac
+}
+createTable
