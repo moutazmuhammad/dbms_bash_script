dae0b3064486e199bc22101d6cc31c7f1e1378b6
Moutaz <moutazmuhammad1997@gmail.com>
2022-01-04 01:39:43 +0200

    fix bug in create table

--- create_table.sh
+++ create_table.sh
@@ -3,13 +3,14 @@ shopt -s extglob
 
 path=./my_dbms/$select_name
 PS3='~> '
+
 function createTable
 {
 	echo -e "\n** Enter Table name: "
 	read -p "~> " table_name
 
-	case $table_name in
-		+([a-zA-Z])) # work by using [shopt -s extglob] lib
+	if [[ $table_name == +([a-zA-Z]) ]]  # work by using [shopt -s extglob] lib
+	then
 			if [ ! -f $path/$table_name ]
 			then
 				touch ./my_dbms/$select_name/$table_name;
@@ -22,88 +23,98 @@ function createTable
 				sleep 1
 				createTable
 			fi
-				;;
-		*) echo -e "\n* Table name can not contain number or spesial characters.\nplease, Try again..."
-		createTable ;;
-	esac
+	else
+			echo -e "\n* Table name can not contain number or spesial characters.\nplease, Try again..."
+			createTable ;
+	fi
 }
 
+((flag=1))
+
+metaData=""
+column_type=""
+ColumnTypeLine=""
 function MetaData
 {
+	sep=":"
+
+	((i=0))
+
 	echo -e '\n** Enter Number of Columns: '
 	read -p "~> " col_num
-	export col_num
-	case  $col_num in
-		+([1-9]))
-			typeset -i index
-	                index=0
-			let i=index
-			let nu=index+1
-			((flag=0))
-			metaData=""
-			column_type=""
-			ColumnTypeLine=""
-			sep=":"
-			newLine="\n"
 
-			while [[ $index -lt $col_num ]]
-			do
+	if [[ $col_num == +([1-9]) ]]
+	then
 
-				if [[ $flag -eq 0 ]]
+		while [[ i -lt $col_num ]]
+		do
+
+			if [[ $flag -eq 1 ]]
+			then
+				echo -e "\n** Enter Primary Key column [$i]:"
+				read -p "~> " col_name
+
+				if [[ $col_name == +([a-zA-Z]) ]]
 				then
-					echo -e "\n** Enter Primary Key column $nu:"
-					read -p "~> " col_name
-					arr[$i]=$col_name
+					select col_type in "Integer" "string"
+					do
+						case $col_type in
+								"Integer") column_type="int"; break;;
+								"string") column_type="str"; break;;
+								*) echo -e "Wrong Choise..\n  You will enter table metadata from begining ... "; 
+						esac
+					done
+					metaData+=$col_name$sep
+					ColumnTypeLine+=$column_type$sep
 					((i++))
-					if [[ $col_name == +([a-zA-Z]) ]]
-					then
-						((flag=1))
-					else
-						echo -e "* Column name must be string.\nYou will enter table metadata from beginning ..."
-						MetaData
-					fi
+					((flag=0))
 				else
-					echo -e "\n** Enter column $nu:"
-                    read -p "~> " col_name
-					arr[$i]=$col_name
-					((i++))
-					if [[ $col_name != +([a-zA-Z]) ]]
-					then
-						echo -e "* Column name must be string.\nYou will enter table metadata from beginning ..."
-						MetaData
-                    fi
+					echo -e "* Column name must be string.\n  You will enter table metadata from beginning ..."
+					continue
 				fi
 
+			else
+
+				if [[ $col_name == +([a-zA-Z]) ]]
+				then
+					echo -e "\n** Enter column [$i]:"
+					read -p "~> " col_name
+
+					if [[ $col_name != +([a-zA-Z]) ]]
+					then
+						echo -e "* Column name must be string.\n  You will enter table metadata from beginning ..."
+						continue
+					else
+
+						select col_type in "Integer" "string"
+						do
+							case $col_type in
+									"Integer") column_type="int"; break;;
+									"string") column_type="str"; break;;
+									*) echo -e "Wrong Choise..\n  You will enter table metadata from begining ... ";
+							esac
+						done
+						metaData+=$col_name$sep
+						ColumnTypeLine+=$column_type$sep
+
+						((i++))
+
+					fi
+				fi
+
+			fi
+
+		done
+
 		
-				select col_type in "Integer" "string"
-				do
 
-					case $col_type in
-							"Integer") column_type="int"; break;;
-							"string") column_type="str"; break;;
-							*) echo -e "Wrong Choise..\nYou will enter table metadata from begining ... "; 
-							((flag=0));
-					esac
+	else
+		MetaData
+	fi
 
-				done
+	echo $metaData >> ./my_dbms/$select_name/$table_name
+	echo $ColumnTypeLine >> ./my_dbms/$select_name/$table_name
 
-				
-				metaData+=$col_name$sep
-				ColumnTypeLine+=$column_type$sep
-
-				((index++))
-				((nu++))
-
-			done
-
-			echo $metaData >> ./my_dbms/$select_name/$table_name
-			echo $ColumnTypeLine >> ./my_dbms/$select_name/$table_name
-			;;
-		*)
-			echo "* You must enter number.."
-			MetaData
-			;;
-	esac
 }
 
 
