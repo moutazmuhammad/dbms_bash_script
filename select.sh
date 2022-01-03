9bb8902651eb3cfebf05a88510bc7b8f555b42b7
Moutaz <moutazmuhammad1997@gmail.com>
2022-01-03 13:20:54 +0200

    edit select script, add: select specific record

--- select.sh
+++ select.sh
@@ -69,12 +69,35 @@ function SelectAllRecords
 function SelectRecord
 {
 
-    echo -e "\n-----------------------"
-    echo -e "** Records"
-    echo -e "-----------------------\n"
-	
-    awk '{if(NR>2) print $0}' ./my_dbms/$select_name/$table_name
+	echo -e "\n** Enter Primary key: "
+    read -p "~> " Pkey
 
+    echo -e "\n----------------------------------------"
+    echo -e "** Record data with Primary key $Pkey "
+    echo -e "----------------------------------------\n"
+
+	Pcheck=`awk -F: '{if ($1 == "'$Pkey'") print 1}' $path/$table_name` # this line check if PK is exist (will print 1)
+	if  [[ $Pcheck -eq 1 ]]
+        then
+            row_num=`awk -F: '{if ($1 == "'$Pkey'"){print NR}}' $path/$table_name` # this line get number of line with primary key $Pkey
+            awk '{if(NR=="'$row_num'") print $0}' $path/$table_name | column -t -s':'
+        else
+		echo "* NO available data!"
+        sleep 1
+	fi
+
+	echo -e "\nPress [b] return to previous menu ."
+	read -p "~> " back
+	while true
+	do
+		if [[ $back == 'b' || $b == 'b' ]]
+		then
+			SelectMenu
+		else
+			echo -e "\nPress [b] return to previous menu ."
+			read -p "~> " b
+		fi
+	done
 }
 
 SelectMenu
