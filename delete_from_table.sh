6f318ad81cc7bba2d9a549ede2433297db515504
Moutaz <moutazmuhammad1997@gmail.com>
2022-01-04 17:18:19 +0200

    fix bug in delete_from_table.sh

--- delete_from_table.sh
+++ delete_from_table.sh
@@ -18,7 +18,7 @@ if [ ! -f $path/$table_name ]
 then
     echo -e "\n* Table ($table_name) is not Exist.\nPlease try again... \n"
     sleep 1
-    . ./use_db.sh
+    . ./delete_from_table.sh
 fi
 
 
@@ -26,18 +26,32 @@ function DeleteFromTable {
        echo -e "\n** Enter Primary key: "
        read -p "~> " Pkey
        Pcheck=`awk -F: '{if ($1 == "'$Pkey'") print 1}' $path/$table_name` # this line check if PK is exist (will print 1)
-       if  [[ $Pcheck -eq 1 ]] 2> /dev/null
+
+       if  [[ $Pcheck -eq 1 ]]
         then
-            row_Del=`awk -F: '{if ($1 == "'$Pkey'"){print NR}}' $path/$table_name` # this line get number of line with primary key $Pkey
-            sed -i ''$row_Del'd' $path/$table_name
-            echo -e "* Data with Primary key $Pkey deleted succefully....\n"
-            sleep 1
+
+            check_row_num=`awk -F: '{if ("'$Pkey'"==$1 && NR<=2) print 1}' $path/$table_name`
+            if [[ $check_row_num -ne 1 ]] # check that the value entered by use != metadata
+            then
+                row_Del=`awk -F: '{if ($1 == "'$Pkey'"){print NR}}' $path/$table_name` # this line get number of line with primary key $Pkey
+                sed -i ''$row_Del'd' $path/$table_name
+                echo -e "* Data with Primary key $Pkey deleted succefully....\n"
+                sleep 1
+                else
+                    echo "* Not allow to delete Metadata!"
+                    sleep 1
+                    DeleteFromTable
+            fi
+
         else
 		echo "* NO available data!"
         sleep 1
+        DeleteFromTable
+
 	fi
 
 }
+
 DeleteFromTable
 . ./use_db.sh
              
